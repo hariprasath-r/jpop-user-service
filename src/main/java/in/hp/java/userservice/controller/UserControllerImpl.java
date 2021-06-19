87fa8a1006e44dcccdb3d2d6d91a3f250584a7c5
Hariprasath R <balaprasad24@gmail.com>
2021-06-19 23:27:28 +0530

    Added controller @Tag

--- src/main/java/in/hp/java/userservice/controller/UserControllerImpl.java
+++ src/main/java/in/hp/java/userservice/controller/UserControllerImpl.java
@@ -4,12 +4,14 @@ import in.hp.java.userservice.controller.contract.UserController;
 import in.hp.java.userservice.dto.UserApiResponse;
 import in.hp.java.userservice.dto.UserDto;
 import in.hp.java.userservice.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User Controller")
 @RestController
 @Slf4j
 public class UserControllerImpl implements UserController {
